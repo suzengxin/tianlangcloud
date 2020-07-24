2dbe776239d71d6492b2e9a0fe76de0f7c506905
suzengxin <38345865+suzengxin@users.noreply.github.com>
2020-07-24 11:15:51 +0800

    创建项目

---
+++ src/main/java/com/tianlang/TianlangApplication.java
@@ -0,0 +1,13 @@
+package com.tianlang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianlangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TianlangApplication.class, args);
+	}
+
+}
